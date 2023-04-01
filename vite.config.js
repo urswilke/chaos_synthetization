961b8a9bfccdfcf85c946f86a3ea69031afbf27c
urswilke <urs.wilke@gmail.com>
2023-04-01 22:12:51 +0200

    try to fix gh actions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/sf2_minimal/',  
+    base: '/sf2_minimal/',  
+    // from here:
+    esbuild: {
+        supported: {
+          'top-level-await': true //browsers can handle top-level-await features
+        },
+      },
 })
