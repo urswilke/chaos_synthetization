0a9d26596d24851d709c9b87d54bdaaca1b87a4f
urswilke <urs.wilke@gmail.com>
2023-04-02 00:39:05 +0200

    play note sequence

--- main.js
+++ main.js
@@ -6,9 +6,29 @@ const sf2 = await loadSoundfont(
 
 function playEndlessNote() {
   const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
-  let preset = 0;
   let midi = 60; 
-  let time = 1;
-  startPresetNote(ctx, sf2.presets[0], midi, time)
+  let time = 0;
+  const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
+  setTimeout(() => {
+    stopHandle((ctx?.currentTime || 0) + 2);
+  }, 1000);
 }
-document.getElementById("play-endless-note-button").addEventListener('click', playEndlessNote);
+function playNote(ctx, midi) {
+  const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
+  setTimeout(() => {
+    stopHandle((ctx.currentTime || 0));
+  }, 500);
+}
+const notes = [60, 62, 64, 65, 67, 67, 60];
+async function playSequence(){
+  const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
+  const sleep = m => new Promise(r => setTimeout(r, m));
+
+  for(const midi of notes){
+    await sleep(500);
+    playNote(ctx, midi)
+  }
+}
+
+
+document.getElementById("play-endless-note-button").addEventListener('click', playSequence.bind(null, notes));
