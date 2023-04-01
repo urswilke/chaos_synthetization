e05c6aa8cc67fd5b0ff6573a0362a1e067a581e9
urswilke <urs.wilke@gmail.com>
2023-04-01 22:27:02 +0200

    add button to prevent: An AudioContext was prevented from starting automatically. It must be created or resumed after a user gesture on the page.
    see: https://stackoverflow.com/questions/66801354/web-audio-api-the-audiocontext-was-not-allowed-to-start-it-must-be-resumed-o

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const sf2 = await loadSoundfont(
   'https://raw.githubusercontent.com/felixroos/felixroos.github.io/main/public/Earthbound_NEW.sf2'
 )
 
-const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
-let preset = 0;
-let midi = 60; 
-let time = 1;
-startPresetNote(ctx, sf2.presets[0], midi, time)
+function playEndlessNote() {
+  const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
+  let preset = 0;
+  let midi = 60; 
+  let time = 1;
+  startPresetNote(ctx, sf2.presets[0], midi, time)
+}
+document.getElementById("play-endless-note-button").addEventListener('click', playEndlessNote);
