55e97952c046911c3e4bf9274f78078753c9fae6
urswilke <urs.wilke@gmail.com>
2023-04-02 16:10:34 +0200

    pass note duration as argument

--- main.js
+++ main.js
@@ -5,28 +5,29 @@ const sf2 = await loadSoundfont(
 )
 
 
-function playNote(ctx, midi) {
+function playNote(ctx, midi, duration) {
   const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
   setTimeout(() => {
     stopHandle((ctx.currentTime || 0));
-  }, 500);
+  }, duration);
 }
-async function playSequence(notes){
+async function playSequence(notes, duration){
   const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
   const sleep = m => new Promise(r => setTimeout(r, m));
 
   for(const midi of notes){
-    await sleep(500);
-    playNote(ctx, midi)
+    await sleep(duration);
+    playNote(ctx, midi, duration)
   }
 }
 
-function playMultipleSequences(l) {
+function playMultipleSequences(l, duration) {
   for (let i = 0; i < l.length; i++) {
-    playSequence(l[i])
+    playSequence(l[i], duration)
   }
 }
 
 const notes1 = [60, 62, 64, 65, 67, 67, 60];
 const notes2 = [64, 65, 67, 67, 60, 60, 62];
-document.getElementById("play-multiple-sequences").addEventListener('click', playMultipleSequences.bind(null, [notes1, notes2]));
+const duration = 500;
+document.getElementById("play-multiple-sequences").addEventListener('click', playMultipleSequences.bind(null, [notes1, notes2], duration));
