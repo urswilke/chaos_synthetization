207f0bc3ff7697204fc72cccaff5fb030e8d5a75
urswilke <urs.wilke@gmail.com>
2023-04-02 17:15:39 +0200

    update vite config base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/sf2_minimal/',  
+    base: '/chaos_synthetization/',  
     // from here:
     esbuild: {
         supported: {
