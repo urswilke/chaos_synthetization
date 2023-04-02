941eda2562c3825918676aa883f951e7605dcdae
urswilke <urs.wilke@gmail.com>
2023-04-02 16:20:35 +0200

    put command in multiple lines

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ import playMultipleSequences from './playNotes.js'
 const notes1 = [60, 62, 64, 65, 67, 67, 60];
 const notes2 = [64, 65, 67, 67, 60, 60, 62];
 const duration = 500;
-document.getElementById("play-multiple-sequences").addEventListener('click', playMultipleSequences.bind(null, [notes1, notes2], duration));
+document
+  .getElementById("play-multiple-sequences")
+  .addEventListener(
+    'click', 
+    playMultipleSequences.bind(
+      null, 
+      [notes1, notes2], 
+      duration
+    )
+  );
