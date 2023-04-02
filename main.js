d2fb12a0733bfed104ec24055eca23347f4a7543
urswilke <urs.wilke@gmail.com>
2023-04-02 16:19:56 +0200

    move code to module

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-import { loadSoundfont, startPresetNote } from 'sfumato';
-
-const sf2 = await loadSoundfont(
-  'https://raw.githubusercontent.com/felixroos/felixroos.github.io/main/public/Earthbound_NEW.sf2'
-)
-
-
-function playNote(ctx, midi, duration) {
-  const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
-  setTimeout(() => {
-    stopHandle((ctx.currentTime || 0));
-  }, duration);
-}
-async function playSequence(notes, duration){
-  const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
-  const sleep = m => new Promise(r => setTimeout(r, m));
-
-  for(const midi of notes){
-    await sleep(duration);
-    playNote(ctx, midi, duration)
-  }
-}
-
-function playMultipleSequences(l, duration) {
-  for (let i = 0; i < l.length; i++) {
-    playSequence(l[i], duration)
-  }
-}
-
+import playMultipleSequences from './playNotes.js'
 const notes1 = [60, 62, 64, 65, 67, 67, 60];
 const notes2 = [64, 65, 67, 67, 60, 60, 62];
 const duration = 500;
---
+++ playNotes.js
@@ -0,0 +1,28 @@
+import { loadSoundfont, startPresetNote } from 'sfumato';
+
+const sf2 = await loadSoundfont(
+  'https://raw.githubusercontent.com/felixroos/felixroos.github.io/main/public/Earthbound_NEW.sf2'
+)
+
+
+function playNote(ctx, midi, duration) {
+  const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
+  setTimeout(() => {
+    stopHandle((ctx.currentTime || 0));
+  }, duration);
+}
+async function playSequence(notes, duration){
+  const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
+  const sleep = m => new Promise(r => setTimeout(r, m));
+
+  for(const midi of notes){
+    await sleep(duration);
+    playNote(ctx, midi, duration)
+  }
+}
+
+export default function playMultipleSequences(l, duration) {
+  for (let i = 0; i < l.length; i++) {
+    playSequence(l[i], duration)
+  }
+}
