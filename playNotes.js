2defa79e50035b1087d397ef129718825f2a9255
urswilke <urs.wilke@gmail.com>
2023-04-19 13:53:32 +0200

    play sounds of selected presets in table

--- playNotes.js
+++ playNotes.js
@@ -2,18 +2,18 @@ import { startPresetNote } from 'sfumato';
 
 
 
-function playNote(ctx, midi, duration, sf2) {
-  const stopHandle = startPresetNote(ctx, sf2.presets[0], midi);
+function playNote(ctx, midi, duration, preset) {
+  const stopHandle = startPresetNote(ctx, preset, midi);
   setTimeout(() => {
     stopHandle((ctx.currentTime || 0));
   }, duration);
 }
-async function playSequence(notes, duration, sf2){
+async function playSequence(notes, duration, preset){
   const ctx = typeof AudioParam !== 'undefined' ? new AudioContext() : null;
   const sleep = m => new Promise(r => setTimeout(r, m));
-
+  
   for(const midi of notes){
-    playNote(ctx, midi, duration, sf2)
+    playNote(ctx, midi, duration, preset)
     await sleep(duration);
   }
 }
@@ -21,7 +21,8 @@ async function playSequence(notes, duration, sf2){
 export default async function playMultipleSequences(l, sf2) {
   let promises = new Array(l.length);
   for (let i = 0; i < l.length; i++) {
-    promises[i] = playSequence(l[i].midi_curve, l[i].duration * l[i].note_length, sf2);
+    let preset = sf2.presets[l[i].preset];
+    promises[i] = playSequence(l[i].midi_curve, l[i].duration * l[i].note_length, preset);
   }
   return promises;
 }
